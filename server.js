6c12f2c5d609db292fb253ec46926af00131d502
makefantastic <makefantastic@gmail.com>
2013-10-06 13:40:09 +0000

    updating files

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var http = require("http");
 var url = require("url");
-
-function start(route) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-
-    route(pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(3000, '0.0.0.0');
-  console.log("Server has started.");
+function start(route, handle) {
+function onRequest(request, response) {
+var postData = "";
+var pathname = url.parse(request.url).pathname;
+console.log("Request for " + pathname + " received.");
+request.setEncoding("utf8");
+request.addListener("data", function(postDataChunk) {
+postData += postDataChunk;
+console.log("Received POST data chunk '"+
+postDataChunk + "'.");
+});
+request.addListener("end", function() {
+route(handle, pathname, response, postData);
+});
 }
-
+  http.createServer(onRequest).listen(3000, '0.0.0.0');
+console.log("Server has started.");
+}
 exports.start = start;
